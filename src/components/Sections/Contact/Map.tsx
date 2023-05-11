d89272d3a851957ab202b5c81506ce0a1566f766
Ernesto Neto <ernestoborges1995@gmail.com>
2023-05-11 14:09:51 -0300

    feat: Added link buttons to googlemaps and waze on Contact > Map; Not functional

--- src/components/Sections/Contact/Map.tsx
+++ src/components/Sections/Contact/Map.tsx
@@ -23,30 +23,39 @@ export function Map() {
             <Container>
                 {
                     isLoaded &&
-                    <GoogleMap
-                        mapContainerStyle={{ width: "100%", height: "100%" }}
-                        center={position}
-                        zoom={17}
-                        options={{
-                            zoomControl: false,
-                            streetViewControl: false,
-                            mapTypeControl: false,
-                            fullscreenControl: false
-                        }}
-                        onLoad={(map) => map.panTo(position)}
-                    >
-                        <MarkerF
-                            position={position}
-                            title="Modelo Engenharia"
-                            label={label}
-                            icon={{
-                                url: "/images/map/marker.png",
-                                scaledSize: new google.maps.Size(25, 40),
-                                labelOrigin: new google.maps.Point(66, 0)
+                    <>
+                        <GoogleMap
+                            mapContainerStyle={{ width: "100%", height: "100%" }}
+                            center={position}
+                            zoom={17}
+                            options={{
+                                zoomControl: false,
+                                streetViewControl: false,
+                                mapTypeControl: false,
+                                fullscreenControl: false
                             }}
-                        />
-                    </GoogleMap>
-
+                            onLoad={(map) => map.panTo(position)}
+                        >
+                            <MarkerF
+                                position={position}
+                                title="Modelo Engenharia"
+                                label={label}
+                                icon={{
+                                    url: "/images/map/marker.png",
+                                    scaledSize: new google.maps.Size(25, 40),
+                                    labelOrigin: new google.maps.Point(66, 0)
+                                }}
+                            />
+                        </GoogleMap>
+                        <ButtonsContainer>
+                            <GoogleMapsRedirect>
+                                Google Maps
+                            </GoogleMapsRedirect>
+                            <WazeRedirect>
+                                Waze
+                            </WazeRedirect>
+                        </ButtonsContainer>
+                    </>
                 }
             </Container>
         </>
@@ -56,6 +65,7 @@ export function Map() {
 const Container = styled.div`
     width: 100%;
     height: 40rem;
+    position: relative;
 
     & .my-custom-class-for-label {
         margin-top: 3.4rem;
@@ -73,4 +83,42 @@ const Container = styled.div`
             0px 1px 0px white,   /* Bottom shadow */
             0px -1px 0px white;  /* Top shadow */
     }
+`
+
+const ButtonsContainer = styled.div`
+    top: 0;
+    left: 0;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+
+`
+
+const Button = styled.button`
+    width: 100%;
+    padding: 1rem 2rem; 
+    border: 0;
+    background-color: var(--black);
+    color: var(--white);
+    border: 0.1rem solid transparent;
+    border-radius: 0.4rem;
+    cursor: pointer;
+    transition: background-color 0.3s, color 0.3s, border 0.3s;
+    
+
+    &:hover {
+        background-color: var(--white);
+        color: var(--black);
+        border: 0.1rem solid var(--black);
+    }
+`
+
+const GoogleMapsRedirect = styled(Button)`
+    
+`
+
+const WazeRedirect = styled(Button)`
+    
 `
